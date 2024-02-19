19aa932f3354e0ae23200fd6cac60e4280b167aa
kaziarman23 <kaziarman@proton.me>
2024-02-19 12:20:02 +0600

    htmlAndCss-complite

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+
+const myLibrary = [
+    
+]
+
+function Book(titel, authorName, pages, read){
+    this.titel = titel,
+    this.authorName = authorName,
+    this.pages = pages,
+    this.read = read
+}
+
+
+function addBooksToLibrary (){
+   
+    
+}
+
